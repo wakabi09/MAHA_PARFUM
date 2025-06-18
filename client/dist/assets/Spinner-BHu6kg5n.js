afb8b871b65efc94720a42e154dc384e3c7a52d1
alfatch09 <alfatch09@gmail.com>
2025-06-18 20:08:12 +0700

    migrasi from sql to mongo

---
+++ client/dist/assets/Spinner-BHu6kg5n.js
@@ -0,0 +1,1 @@
+import{r as i,b as m,j as x,c as $}from"./index-DMg7K7Es.js";const d=i.forwardRef(({bsPrefix:s,variant:r,animation:t="border",size:e,as:n="div",className:o,...p},c)=>{s=m(s,"spinner");const a=`${s}-${t}`;return x.jsx(n,{ref:c,...p,className:$(o,a,e&&`${a}-${e}`,r&&`text-${r}`)})});d.displayName="Spinner";export{d as S};
